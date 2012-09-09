2a50c84b9fee42de7a2b43625fef66dd574611eb
Mark Vensawhl <kylerob89@gmail.com>
2012-09-09 09:42:55 -0400

    fix

--- recall.rb
+++ recall.rb
@@ -16,7 +16,7 @@ class Note
 	include DataMapper::Resource
 	property :id, Serial
 	property :content, Text, :required => true
-	property :complete, Boolean, :required => true, :default => 0
+	property :complete, Boolean, :required => true, :default => false
 	property :created_at, DateTime
 	property :updated_at, DateTime
 end
