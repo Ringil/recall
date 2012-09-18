8012658892b36d32c93fd1f216ed27c23e8de336
Mark Vensawhl <kylerob89@gmail.com>
2012-09-18 00:00:43 -0400

    revert

--- recall.rb
+++ recall.rb
@@ -66,7 +66,8 @@ end
 
 get '/' do
     if current_user
-        @notes = User.get(session[:user_id]).Note.all :order => :id.desc
+        #@notes = User.get(session[:user_id]).Note.all :order => :id.desc
+        @notes = Note.all :order => :id.desc
 	    @title = 'All Notes'
 	    if @notes.empty?
 		    flash[:error] = 'No notes found. Add your first below.'
